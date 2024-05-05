eb885425ea0434d2a50222e9fc9980f8e56e2f82
NC <nick.courtney22@gmail.com>
2024-05-05 15:20:41 -0400

    Add cut off table

--- will-i-qualify/app.py
+++ will-i-qualify/app.py
@@ -25,20 +25,9 @@ def main():
     male_cutoffs, female_cutoffs, x_cutoffs = fetch_cutoff_data()
 
     # Get user input for age and gender
-    col1, col2, col3 = st.columns([1,1,1])
+    col1, col2 = st.columns([1,1])
     gender = col1.radio("Select gender:", ('Male', 'Female'))
     age = col2.number_input("Enter age on date of Boston Marathon (4/25/25):", min_value=18, max_value=110, step=1)
-    application_rate = col3.selectbox("Select rate of runners who qualify for Boston that will actually try to apply", ["Low", "Medium", "High"])
-    if application_rate == "Low":
-        application_integer = 76
-    if application_rate == "Medium":
-        application_integer = 78
-    if application_rate == "High":
-        application_integer = 80
-    application_percentage = application_integer / 100
-
-    # Calculate current cutoff time
-    total_runners_qualified, cutoff_time = run_cutoff_workflow(application_percentage, FIELD_SIZE, BQ_YEAR)
 
     # Get the standard cutoff time for the user's age group
     if gender == "Male":
@@ -46,21 +35,35 @@ def main():
     elif gender == "Female":
         standard_cutoff = female_cutoffs.loc[male_cutoffs["Age"] == age, "F"]
     standard_cutoff_time = standard_cutoff.iloc[0]
-
-    # Calculate the adjusted cutoff time for the user's age group
     standard_cutoff_delta = convert_to_timedelta(standard_cutoff_time)
-    cutoff_time_delta = convert_to_timedelta(cutoff_time)
-    adjusted_cutoff_time = standard_cutoff_delta - cutoff_time_delta
 
     button = st.button("Calculate Cutoff")
     st.write("----------")
     if button:
+
+        application_rates = [.77, .8, .83, 1]
+        application_rate_labels = [["Everyone Applies", "More than Usual Apply", "Average Number Apply", "Less than Usual Apply"]]
+        # Create cutoff table and projected qualifying times based on application rates
+        df = pd.DataFrame(columns=["Cut-Off Time", "Projected Qualifying Time"])
+        for rate in application_rates:
+            total_runners_qualified, cutoff_time = run_cutoff_workflow(rate, FIELD_SIZE, BQ_YEAR)
+
+            cutoff_time_delta = convert_to_timedelta(cutoff_time)
+            adjusted_cutoff_time = standard_cutoff_delta - cutoff_time_delta
+            df.loc[-1] = [cutoff_time, str(adjusted_cutoff_time)]
+            df.index = df.index + 1
+            df = df.sort_index()
+        df.set_index(application_rate_labels, drop=True, inplace=True)
+
         # Display result
-        st.html("<h3>Results</h3>")
-        st.write("Total qualified runners:", str(total_runners_qualified))
-        st.write("Current cutoff time:", cutoff_time)
-        st.write(f"Standard cutoff time for an age {age} {gender}: {standard_cutoff_time}")
-        st.write(f"Adjusted cutoff time: {adjusted_cutoff_time}")
+        st.html(f"""<h3>Results</h3>
+            <b>Total qualified runners:</b> {str(total_runners_qualified)}<br>
+            <b>Official Qualifying Time for an Age {age} {gender}</b>: {standard_cutoff_time}<br><br>
+            
+            Projected cut-off and qualifying times based on the rate at which people who qualify for Boston actually apply for it
+            """
+        )
+        st.write(df)
 
 
 if __name__ == "__main__":
